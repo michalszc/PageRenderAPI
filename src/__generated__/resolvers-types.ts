4d1db06fdc736751ebe76ad2d922809cd6247ac5
Michal <michalszc05@gmail.com>
2023-08-27 23:21:40 +0200

    Modified schema

--- src/__generated__/resolvers-types.ts
+++ src/__generated__/resolvers-types.ts
@@ -1,5 +1,7 @@
+/* eslint-disable */
 import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
 import { Context } from '../utils/setup';
+import * as E from '../utils/errors';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -15,12 +17,21 @@ export type Scalars = {
   Boolean: { input: boolean; output: boolean; }
   Int: { input: number; output: number; }
   Float: { input: number; output: number; }
-  /** Represents a date value in ISO 8601 format. */
+  /**
+   * A date string, such as 2007-12-03, compliant with the `full-date`
+   * format outlined in section 5.6 of the RFC 3339 profile of the
+   * ISO 8601 standard for representation of dates and times using
+   * the Gregorian calendar.
+   */
   Date: { input: string; output: string; }
-  /** Represents a URL string. */
+  /** A field whose value conforms to the standard URL format as specified in RFC3986: https://www.ietf.org/rfc/rfc3986.txt. */
   URL: { input: string; output: string; }
-  /** Represents a universally unique identifier (UUID). */
+  /** A field whose value is a generic Universally Unique Identifier: https://en.wikipedia.org/wiki/Universally_unique_identifier. */
   UUID: { input: string; output: string; }
+};
+
+export type BaseError = {
+  message: Scalars['String']['output'];
 };
 
 export type CreatePageInput = {
@@ -39,6 +50,18 @@ export type DateFilter = {
   lt?: InputMaybe<Scalars['Date']['input']>;
   /** Filters for dates less than or equal to the specified value. */
   lte?: InputMaybe<Scalars['Date']['input']>;
+};
+
+export type InputFieldValidation = {
+  __typename?: 'InputFieldValidation';
+  field: Scalars['String']['output'];
+  message: Scalars['String']['output'];
+};
+
+export type InvalidInputError = BaseError & {
+  __typename?: 'InvalidInputError';
+  inputs: Array<InputFieldValidation>;
+  message: Scalars['String']['output'];
 };
 
 export type Mutation = {
@@ -65,6 +88,11 @@ export type MutationDeletePageArgs = {
 export type MutationUpdatePageArgs = {
   id: Scalars['UUID']['input'];
   input: UpdatePageInput;
+};
+
+export type NotFoundError = BaseError & {
+  __typename?: 'NotFoundError';
+  message: Scalars['String']['output'];
 };
 
 export type Page = {
@@ -108,6 +136,8 @@ export type PageInfo = {
   startCursor?: Maybe<Scalars['UUID']['output']>;
 };
 
+export type PageResult = InvalidInputError | NotFoundError | Page | UnknownError;
+
 export type PageSortInput = {
   /** Specify the field to sort by */
   field: SortFieldEnum;
@@ -145,12 +175,14 @@ export type Pages = {
   pageInfo: PageInfo;
 };
 
+export type PagesResult = InvalidInputError | NotFoundError | Pages | UnknownError;
+
 export type Query = {
   __typename?: 'Query';
   /** Retrieves a specific page by its ID. */
-  page: Page;
+  page: PageResult;
   /** Retrieves a list of pages. */
-  pages: Pages;
+  pages: PagesResult;
 };
 
 
@@ -204,6 +236,11 @@ export enum SortOrderEnum {
   /** Sort in descending order */
   Desc = 'DESC'
 }
+
+export type UnknownError = BaseError & {
+  __typename?: 'UnknownError';
+  message: Scalars['String']['output'];
+};
 
 export type UpdatePageInput = {
   /** The URL of the website for the new page. */
@@ -280,24 +317,39 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
   info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
+/** Mapping of union types */
+export type ResolversUnionTypes<RefType extends Record<string, unknown>> = ResolversObject<{
+  PageResult: ( E.WrappedError<E.InvalidInputError> ) | ( E.WrappedError<E.NotFoundError> ) | ( Page ) | ( E.WrappedError<E.UnknownError> );
+  PagesResult: ( E.WrappedError<E.InvalidInputError> ) | ( E.WrappedError<E.NotFoundError> ) | ( Pages ) | ( E.WrappedError<E.UnknownError> );
+}>;
 
+/** Mapping of interface types */
+export type ResolversInterfaceTypes<RefType extends Record<string, unknown>> = ResolversObject<{
+  BaseError: ( E.WrappedError<E.InvalidInputError> ) | ( E.WrappedError<E.NotFoundError> ) | ( E.WrappedError<E.UnknownError> );
+}>;
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = ResolversObject<{
+  BaseError: ResolverTypeWrapper<ResolversInterfaceTypes<ResolversTypes>['BaseError']>;
   Boolean: ResolverTypeWrapper<Scalars['Boolean']['output']>;
   CreatePageInput: CreatePageInput;
   Date: ResolverTypeWrapper<Scalars['Date']['output']>;
   DateFilter: DateFilter;
+  InputFieldValidation: ResolverTypeWrapper<E.InputFieldError>;
   Int: ResolverTypeWrapper<Scalars['Int']['output']>;
+  InvalidInputError: ResolverTypeWrapper<E.WrappedError<E.InvalidInputError>>;
   Mutation: ResolverTypeWrapper<{}>;
+  NotFoundError: ResolverTypeWrapper<E.WrappedError<E.NotFoundError>>;
   Page: ResolverTypeWrapper<Page>;
   PageEdge: ResolverTypeWrapper<PageEdge>;
   PageFilterInput: PageFilterInput;
   PageInfo: ResolverTypeWrapper<PageInfo>;
+  PageResult: ResolverTypeWrapper<ResolversUnionTypes<ResolversTypes>['PageResult']>;
   PageSortInput: PageSortInput;
   PageTypeEnum: PageTypeEnum;
   PageTypeEnumFilter: PageTypeEnumFilter;
   Pages: ResolverTypeWrapper<Pages>;
+  PagesResult: ResolverTypeWrapper<ResolversUnionTypes<ResolversTypes>['PagesResult']>;
   Query: ResolverTypeWrapper<{}>;
   Result: ResolverTypeWrapper<Result>;
   ResultStatusEnum: ResultStatusEnum;
@@ -306,40 +358,70 @@ export type ResolversTypes = ResolversObject<{
   String: ResolverTypeWrapper<Scalars['String']['output']>;
   URL: ResolverTypeWrapper<Scalars['URL']['output']>;
   UUID: ResolverTypeWrapper<Scalars['UUID']['output']>;
+  UnknownError: ResolverTypeWrapper<E.WrappedError<E.UnknownError>>;
   UpdatePageInput: UpdatePageInput;
 }>;
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = ResolversObject<{
+  BaseError: ResolversInterfaceTypes<ResolversParentTypes>['BaseError'];
   Boolean: Scalars['Boolean']['output'];
   CreatePageInput: CreatePageInput;
   Date: Scalars['Date']['output'];
   DateFilter: DateFilter;
+  InputFieldValidation: E.InputFieldError;
   Int: Scalars['Int']['output'];
+  InvalidInputError: E.WrappedError<E.InvalidInputError>;
   Mutation: {};
+  NotFoundError: E.WrappedError<E.NotFoundError>;
   Page: Page;
   PageEdge: PageEdge;
   PageFilterInput: PageFilterInput;
   PageInfo: PageInfo;
+  PageResult: ResolversUnionTypes<ResolversParentTypes>['PageResult'];
   PageSortInput: PageSortInput;
   PageTypeEnumFilter: PageTypeEnumFilter;
   Pages: Pages;
+  PagesResult: ResolversUnionTypes<ResolversParentTypes>['PagesResult'];
   Query: {};
   Result: Result;
   String: Scalars['String']['output'];
   URL: Scalars['URL']['output'];
   UUID: Scalars['UUID']['output'];
+  UnknownError: E.WrappedError<E.UnknownError>;
   UpdatePageInput: UpdatePageInput;
+}>;
+
+export type BaseErrorResolvers<ContextType = Context, ParentType extends ResolversParentTypes['BaseError'] = ResolversParentTypes['BaseError']> = ResolversObject<{
+  __resolveType: TypeResolveFn<'InvalidInputError' | 'NotFoundError' | 'UnknownError', ParentType, ContextType>;
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
 }>;
 
 export interface DateScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['Date'], any> {
   name: 'Date';
 }
 
+export type InputFieldValidationResolvers<ContextType = Context, ParentType extends ResolversParentTypes['InputFieldValidation'] = ResolversParentTypes['InputFieldValidation']> = ResolversObject<{
+  field?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type InvalidInputErrorResolvers<ContextType = Context, ParentType extends ResolversParentTypes['InvalidInputError'] = ResolversParentTypes['InvalidInputError']> = ResolversObject<{
+  inputs?: Resolver<Array<ResolversTypes['InputFieldValidation']>, ParentType, ContextType>;
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
 export type MutationResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = ResolversObject<{
   createPage?: Resolver<ResolversTypes['Result'], ParentType, ContextType, RequireFields<MutationCreatePageArgs, 'input'>>;
   deletePage?: Resolver<ResolversTypes['Result'], ParentType, ContextType, RequireFields<MutationDeletePageArgs, 'id'>>;
   updatePage?: Resolver<ResolversTypes['Result'], ParentType, ContextType, RequireFields<MutationUpdatePageArgs, 'id' | 'input'>>;
+}>;
+
+export type NotFoundErrorResolvers<ContextType = Context, ParentType extends ResolversParentTypes['NotFoundError'] = ResolversParentTypes['NotFoundError']> = ResolversObject<{
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
 export type PageResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Page'] = ResolversParentTypes['Page']> = ResolversObject<{
@@ -365,15 +447,23 @@ export type PageInfoResolvers<ContextType = Context, ParentType extends Resolver
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
+export type PageResultResolvers<ContextType = Context, ParentType extends ResolversParentTypes['PageResult'] = ResolversParentTypes['PageResult']> = ResolversObject<{
+  __resolveType: TypeResolveFn<'InvalidInputError' | 'NotFoundError' | 'Page' | 'UnknownError', ParentType, ContextType>;
+}>;
+
 export type PagesResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Pages'] = ResolversParentTypes['Pages']> = ResolversObject<{
   edges?: Resolver<Array<ResolversTypes['PageEdge']>, ParentType, ContextType>;
   pageInfo?: Resolver<ResolversTypes['PageInfo'], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
+export type PagesResultResolvers<ContextType = Context, ParentType extends ResolversParentTypes['PagesResult'] = ResolversParentTypes['PagesResult']> = ResolversObject<{
+  __resolveType: TypeResolveFn<'InvalidInputError' | 'NotFoundError' | 'Pages' | 'UnknownError', ParentType, ContextType>;
+}>;
+
 export type QueryResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = ResolversObject<{
-  page?: Resolver<ResolversTypes['Page'], ParentType, ContextType, RequireFields<QueryPageArgs, 'id'>>;
-  pages?: Resolver<ResolversTypes['Pages'], ParentType, ContextType, Partial<QueryPagesArgs>>;
+  page?: Resolver<ResolversTypes['PageResult'], ParentType, ContextType, RequireFields<QueryPageArgs, 'id'>>;
+  pages?: Resolver<ResolversTypes['PagesResult'], ParentType, ContextType, Partial<QueryPagesArgs>>;
 }>;
 
 export type ResultResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Result'] = ResolversParentTypes['Result']> = ResolversObject<{
@@ -392,16 +482,28 @@ export interface UuidScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes
   name: 'UUID';
 }
 
+export type UnknownErrorResolvers<ContextType = Context, ParentType extends ResolversParentTypes['UnknownError'] = ResolversParentTypes['UnknownError']> = ResolversObject<{
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
 export type Resolvers<ContextType = Context> = ResolversObject<{
+  BaseError?: BaseErrorResolvers<ContextType>;
   Date?: GraphQLScalarType;
+  InputFieldValidation?: InputFieldValidationResolvers<ContextType>;
+  InvalidInputError?: InvalidInputErrorResolvers<ContextType>;
   Mutation?: MutationResolvers<ContextType>;
+  NotFoundError?: NotFoundErrorResolvers<ContextType>;
   Page?: PageResolvers<ContextType>;
   PageEdge?: PageEdgeResolvers<ContextType>;
   PageInfo?: PageInfoResolvers<ContextType>;
+  PageResult?: PageResultResolvers<ContextType>;
   Pages?: PagesResolvers<ContextType>;
+  PagesResult?: PagesResultResolvers<ContextType>;
   Query?: QueryResolvers<ContextType>;
   Result?: ResultResolvers<ContextType>;
   URL?: GraphQLScalarType;
   UUID?: GraphQLScalarType;
+  UnknownError?: UnknownErrorResolvers<ContextType>;
 }>;
 
